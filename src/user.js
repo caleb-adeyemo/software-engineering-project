a1f3adc47cda6ac06e3b0749f450c5de13c473bf
caleb <66700303+caleb-adeyemo@users.noreply.github.com>
2022-03-17 18:12:22 +0000

    Added comments

--- src/user.js
+++ src/user.js
@@ -1,3 +1,4 @@
-export function User(username,name,email){
-   return {username: username , name: name , email: email};   
+// Create users object
+export function User(username, name, email) {
+    return { username: username, name: name, email: email };
 }
