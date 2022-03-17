be25721be9f5cc29f0c84811a8e6272a586a6925
Carine <carine3338@gmail.com>
2022-03-17 20:43:59 +0000

    test-branch

--- src/js/reservation.js
+++ src/js/reservation.js
@@ -1,68 +1,70 @@
-
-const assert = require('assert/strict');
-const util = require ('./dbms.js');
+const assert = require("assert/strict");
+const util = require("./dbms.js");
 
 const TIME_COLL = Symbol("times overlap"); // Return values
 const INV_USER = Symbol("invalid user"); // Return values
 const RES_OK = Symbol("reservation is vaild"); // Return values
 module.exports.duration = duration;
 module.exports.time = time;
-module.exports.reservation = reservation; 
+module.exports.reservation = reservation;
 module.exports.is_valid = is_valid;
-module.exports.TIME_COLL = TIME_COLL; 
-module.exports.INV_USER = INV_USER; 
-module.exports.RES_OK = RES_OK; 
-   
+module.exports.TIME_COLL = TIME_COLL;
+module.exports.INV_USER = INV_USER;
+module.exports.RES_OK = RES_OK;
+
 // Ensures the input duration are whole nubers not floating points (i.e. 1.5hrs)
 
 function duration(hr, min) {
-    assert.deepEqual(Number.isInteger(hr), true, 'hours must be a whole number');
-    assert.deepEqual(Number.isInteger(min), true, 'minutes must be a whole number');
-    return { hr: hr, min: min }
+	assert.deepEqual(Number.isInteger(hr), true, "hours must be a whole number");
+	assert.deepEqual(
+		Number.isInteger(min),
+		true,
+		"minutes must be a whole number"
+	);
+	return { hr: hr, min: min };
 }
 
-//returns time object  
+//returns time object
 function time(_start, _dur) {
-    let start = new Date(
-        _start.getFullYear(),
-        _start.getMonth(),
-        _start.getDay(),
-        _start.getHours(),
-        _start.getMinutes(),
-    );
+	let start = new Date(
+		_start.getFullYear(),
+		_start.getMonth(),
+		_start.getDay(),
+		_start.getHours(),
+		_start.getMinutes()
+	);
 
-    // checks to see if the time periods overlaps 
-    function overlap(t1) {
-        console.log('t0: ' + JSON.stringify(start));
-        console.log('t1: ' + JSON.stringify(t1));
-        start.setHours(start.getHours() + _dur.hr);
-        start.setMinutes(start.getMinutes() + _dur.min);
-        let res = (start > t1.start);
-        console.log('t0 > t1: ' + res + '\n');
-        //reset start 
-        start.setHours(start.getHours() - _dur.hr);
-        start.setMinutes(start.getMinutes() - _dur.min);
+	// checks to see if the time periods overlaps
+	function overlap(t1) {
+		console.log("t0: " + JSON.stringify(start));
+		console.log("t1: " + JSON.stringify(t1));
+		start.setHours(start.getHours() + _dur.hr);
+		start.setMinutes(start.getMinutes() + _dur.min);
+		let res = start > t1.start;
+		console.log("t0 > t1: " + res + "\n");
+		//reset start
+		start.setHours(start.getHours() - _dur.hr);
+		start.setMinutes(start.getMinutes() - _dur.min);
 
-        return res;
-    }
+		return res;
+	}
 
-    return { start: start, overlap: overlap };
-
+	return { start: start, overlap: overlap };
 }
 
 // Creates reservation object
 function reservation(time, space, user, number_plate) {
-    return { time: time, space: space, user: user, number_plate };
+	return { time: time, space: space, user: user, number_plate };
 }
 
 // checks to see if reservations clashes/vaild
- function is_valid(res_arr, new_resrv) {
-    const collisions = res_arr.filter(col => col.space === new_resrv.space);
-    for (let i = 0; i < collisions.length; i++) {
-        if (collisions[i].time.overlap(new_resrv.time) === true) {
-            console.log(TIME_COLL);
-            return util.Result(TIME_COLL, 'space already booked', null);
-        }
-    }
-    return util.Result(RES_OK, 'resvation made successfully', null);
+function is_valid(res_arr, new_resrv) {
+	const collisions = res_arr.filter((col) => col.space === new_resrv.space);
+	for (let i = 0; i < collisions.length; i++) {
+		if (collisions[i].time.overlap(new_resrv.time) === true) {
+			console.log(TIME_COLL);
+			return util.Result(TIME_COLL, "space already booked", null);
+		}
+	}
+	return util.Result(RES_OK, "resvation made successfully", null);
 }
--- src/js/space.js
+++ src/js/space.js
@@ -2,26 +2,26 @@ module.exports.space = space;
 module.exports.init_lot = init_lot;
 // Creates Space object
 function space(level, bay) {
-    return { level: level, bay: bay };
+	return { level: level, bay: bay };
 }
-// Inistialsie the parking lot 
+// Inistialsie the parking lot
 function init_lot(levels, bays) {
-    let spaces = [];
-    let level = 'A';
-    let bay_count = 0;
+	let spaces = [];
+	let level = "A";
+	let bay_count = 0;
 
-    const n_spaces = levels * bays; //Number of bays 
+	const n_spaces = levels * bays; //Number of bays
 
-    let parking_spot;
+	let parking_spot;
 
-    for (let i = 0; i < n_spaces; i++) {
-        parking_spot = space(level, bay_count.toString());
-        bay_count++;
-        if (bay_count === bays) {
-            bay_count = 0;
-            level = String.fromCharCode(level.charCodeAt() + 1); // Increment the Level A -> B
-        }
-        spaces.push(parking_spot); // Add to the array
-    }
-    return spaces;
+	for (let i = 0; i < n_spaces; i++) {
+		parking_spot = space(level, bay_count.toString());
+		bay_count++;
+		if (bay_count === bays) {
+			bay_count = 0;
+			level = String.fromCharCode(level.charCodeAt() + 1); // Increment the Level A -> B
+		}
+		spaces.push(parking_spot); // Add to the array
+	}
+	return spaces;
 }
