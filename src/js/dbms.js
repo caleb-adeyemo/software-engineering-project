4b37833019d92befdd3d952d734584cd185e1cd2
ChiedozieOkoh <chiedozieokoh1@gmail.com>
2022-04-02 20:12:03 +0100

    Implementated serialisable database tables via hashmaps
    any object that you would like to store in it must implement its own
    key() function.

--- src/js/dbms.js
+++ src/js/dbms.js
@@ -1,3 +1,80 @@
+import * as fs from 'fs'; 
+
+export const SOME = Symbol("query result is not empty");
+export const NONE = Symbol("query result is empty,maybe null");
 export function Result(code,msg,unwrap){
    return {code,msg,unwrap};
 }
+
+export function Table(filename){
+   let entries = new Map();
+   const name = filename;
+
+   function add(new_element){
+      entries.set(new_element.key(),new_element);
+   }
+   function set(key,value){
+      entries.set(key,value);
+   }
+   
+   function get(key){
+      return entries.get(key);
+   }
+
+   function contains(key){
+      return entries.has(key);
+   }
+
+   function remove(key){
+      return entries.delete(key);
+   }
+
+   function filter_table(filter_fn){
+      let passed = [];
+
+      for(const entry of entries.values()){
+         if(filter_fn(entry)){
+            passed.push(entry);
+         }
+      }
+      return passed;
+   }
+
+   function serialise(){
+      let key_pairs = Array.from(entries.entries());
+      return JSON.stringify(key_pairs,null,3);
+   }
+   
+   function save_to_file(serialised_item){
+      fs.writeFileSync(name,serialised_item); 
+   }
+
+   return {
+      add:add,
+      get:get, 
+      set:set,
+      contains:contains,
+      remove:remove,
+      filter_table:filter_table,
+      serialise:serialise,
+      save_to_file:save_to_file,
+      load_from_file:load_from_file
+   };
+}
+function deserialise(path,string){
+   let deserialised_map = new Map(JSON.parse(string));
+   console.log(deserialised_map);
+   let new_table = Table(path);
+   for(const entry of deserialised_map.entries()){
+      new_table.set(entry[0],entry[1]);
+      console.log(entry[0]);
+      console.log('result: ' + JSON.stringify(new_table.get(entry[0])));
+   }
+   return new_table;
+}
+export function load_from_file(path){
+   const data = fs.readFileSync(path,'utf8'); 
+   return deserialise(path,data);
+   return data; 
+}
+
--- src/js/user.js
+++ src/js/user.js
@@ -1,4 +1,7 @@
 // Create users object
 export function User(username, name, email) {
-	return { username: username, name: name, email: email };
+      function key(){
+         return username.concat(email);
+      }
+	return { username: username, name: name, email: email, key: key};
 }
